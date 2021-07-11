b58b214f3949773d1656dc275687191172b8ff65
Terry <choir0118@gmail.com>
2021-07-11 22:28:05 +0900

    Feat : Notification 클릭 PendingIntent 기능 추가
    Signed-off-by: Terry <choir0118@gmail.com>

--- features/Notification/src/main/java/com/terry/notification/MyFirebaseMessagingService.kt
+++ features/Notification/src/main/java/com/terry/notification/MyFirebaseMessagingService.kt
@@ -3,7 +3,10 @@ package com.terry.notification
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
@@ -53,16 +56,27 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
+    // 아래 웹에서 push 알림을 테스트할 수 있다.
+    // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages/send?hl=ko&apix_params=%7B%22parent%22%3A%22projects%2Fandroid-standard-level%22%2C%22resource%22%3A%7B%22message%22%3A%7B%22token%22%3A%22exb37naMS72UeNKcNI7MEf%3AAPA91bE4_VZjjgH85eRvXIqelQPT5Nyz1Vxqx_2BJYgQKjw5j4G3npoV-SFU6_QL4rpN2jpp3I4EjSa4-rXSCKBxk5IudENwa7sysfixN61jcCvXJpQ7Lb_niyXNnhJhymXfoRkllchH%22%2C%22data%22%3A%7B%22title%22%3A%22Test%20Title%22%2C%22message%22%3A%22Test%20Message%22%2C%22type%22%3A%22CUSTOM%22%7D%7D%7D%7D
     private fun createNotification(
         type: NotificationType,
         title: String?,
         message: String?
     ): Notification {
+        val intent = Intent(this, NotificationMainActivity::class.java).apply {
+            putExtra("notificationType", "${type.title} 타입")
+            addFlags((Intent.FLAG_ACTIVITY_SINGLE_TOP))
+        }
+        val pendingIntent = PendingIntent.getActivity(this, type.id, intent, FLAG_UPDATE_CURRENT)
+//        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notifications)
             .setContentTitle(title)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         when (type) {
             NotificationType.NORMAL -> Unit
--- features/Notification/src/main/java/com/terry/notification/NotificationMainActivity.kt
+++ features/Notification/src/main/java/com/terry/notification/NotificationMainActivity.kt
@@ -1,5 +1,7 @@
 package com.terry.notification
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import com.google.firebase.messaging.FirebaseMessaging
 import com.terry.common.base.BaseActivity
@@ -20,6 +22,13 @@ class NotificationMainActivity :
         super.onCreate(savedInstanceState)
 
         initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        updateResult(true)
     }
 
     private fun initFirebase() {
@@ -29,5 +38,15 @@ class NotificationMainActivity :
                     firebaseTokenTextView.text = task.result
                 }
             }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
     }
 }
