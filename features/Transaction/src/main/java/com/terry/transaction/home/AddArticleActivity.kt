bd34f4da3e0e16430957505309ddd8ac5184993a
Terry <choir0118@gmail.com>
2021-07-21 23:45:10 +0900

    Feat : 이미지 업로드 기능 추가
    - Firebase Storage 로 기능 구현
    
    Signed-off-by: Terry <choir0118@gmail.com>

--- features/Transaction/src/main/java/com/terry/transaction/home/AddArticleActivity.kt
+++ features/Transaction/src/main/java/com/terry/transaction/home/AddArticleActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
@@ -24,7 +25,9 @@ class AddArticleActivity :
 
     private var selectedUri: Uri? = null
     private val auth by lazy { Firebase.auth }
+
     private val storage by lazy { Firebase.storage }
+
     private val articleDB by lazy { Firebase.database.reference.child(FirebaseDBKey.ARTICLES) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,18 +60,59 @@ class AddArticleActivity :
             val price = binding.priceEditText.text.toString()
             val sellerId = auth.currentUser?.uid.orEmpty()
 
-            val model = ArticleModel(
-                sellerId,
-                title,
-                System.currentTimeMillis(),
-                "$price 원",
-                ""
-            )
+            showProgress()
 
-            articleDB.push().setValue(model)
-
-            finish()
+            if (selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                uploadPhoto(photoUri,
+                    successHandler = { uri ->
+                        uploadArticle(sellerId, title, price, uri)
+                    },
+                    errorHandler = {
+                        Snackbar.make(binding.root, "사진 업로드에 실패했습니다.", Snackbar.LENGTH_SHORT).show()
+                        hideProgress()
+                    }
+                )
+            } else {
+                uploadArticle(sellerId, title, price, "")
+            }
         }
+    }
+
+    private fun uploadPhoto(uri: Uri, successHandler: (String) -> Unit, errorHandler: () -> Unit) {
+        val fileName = "${System.currentTimeMillis()}.png"
+        storage.reference.child("article/photo").child(fileName)
+            .putFile(uri)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    storage.reference.child("article/photo").child(fileName)
+                        .downloadUrl
+                        .addOnSuccessListener { uri ->
+                            successHandler(uri.toString())
+                        }.addOnFailureListener {
+                            errorHandler()
+                        }
+
+                } else {
+                    errorHandler()
+                }
+            }
+    }
+
+    private fun uploadArticle(sellerId: String, title: String, price: String, imageUrl: String) {
+        val model = ArticleModel(
+            sellerId,
+            title,
+            System.currentTimeMillis(),
+            "$price 원",
+            imageUrl
+        )
+
+        articleDB.push().setValue(model)
+
+        hideProgress()
+
+        finish()
     }
 
     override fun onRequestPermissionsResult(
@@ -93,6 +137,14 @@ class AddArticleActivity :
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         startActivityForResult(intent, CONTENT_REQUEST_CODE)
+    }
+
+    private fun showProgress() {
+        binding.progressBar.isVisible = true
+    }
+
+    private fun hideProgress() {
+        binding.progressBar.isVisible = false
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
