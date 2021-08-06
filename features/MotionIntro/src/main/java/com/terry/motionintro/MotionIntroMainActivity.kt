b06bd7a952a259ea7a254eda599868b26ab91dc4
Terry <choir0118@gmail.com>
2021-08-06 23:56:32 +0900

    Feat : Initialize MotionIntro Module
    Signed-off-by: Terry <choir0118@gmail.com>

---
+++ features/MotionIntro/src/main/java/com/terry/motionintro/MotionIntroMainActivity.kt
@@ -0,0 +1,11 @@
+package com.terry.motionintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionIntroMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_intro_main)
+    }
+}
