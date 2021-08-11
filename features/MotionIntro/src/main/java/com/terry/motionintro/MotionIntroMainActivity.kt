1792d40ef14489d0e2716d9e67937ec5ba0139cb
Terry <choir0118@gmail.com>
2021-08-11 21:09:03 +0900

    Feat : Initialize Appbar & Image Motion layout
    Signed-off-by: Terry <choir0118@gmail.com>

--- features/MotionIntro/src/main/java/com/terry/motionintro/MotionIntroMainActivity.kt
+++ features/MotionIntro/src/main/java/com/terry/motionintro/MotionIntroMainActivity.kt
@@ -1,11 +1,167 @@
 package com.terry.motionintro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.AppBarLayout
+import com.terry.common.LogT
+import com.terry.common.base.BaseActivity
+import com.terry.motionintro.databinding.ActivityMotionIntroMainBinding
+import kotlin.math.abs
 
-class MotionIntroMainActivity : AppCompatActivity() {
+class MotionIntroMainActivity :
+    BaseActivity<ActivityMotionIntroMainBinding>(ActivityMotionIntroMainBinding::inflate) {
+
+    private var isGatheringMotionAnimation: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_motion_intro_main)
+
+        makeStatusBarTransparent()
+
+        initAppBar()
+
+        initInsetMargin()
+        initScrollViewListeners()
+
+        initMotionLayoutListeners()
+    }
+
+    private fun initAppBar() {
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val topPadding = 300f.dpToPx(this)
+            val realAlphaScrollHeight = appBarLayout.measuredHeight - appBarLayout.totalScrollRange
+            val abstractOffset = abs(verticalOffset)
+
+            val realAlphaVerticalOffset =
+                if (abstractOffset - topPadding < 0) 0f else abstractOffset - topPadding
+
+            if (abstractOffset < topPadding) {
+                binding.toolbarBackgroundView.alpha = 0f
+                return@OnOffsetChangedListener
+            }
+            val percentage = realAlphaVerticalOffset / realAlphaScrollHeight
+            binding.toolbarBackgroundView.alpha =
+                1 - (if (1 - percentage * 2 < 0) 0f else 1 - percentage * 2)
+        })
+        initActionBar()
+    }
+
+    private fun initActionBar() = with(binding) {
+        toolbar.navigationIcon = null
+        toolbar.setContentInsetsAbsolute(0, 0)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setHomeButtonEnabled(false)
+            it.setDisplayHomeAsUpEnabled(false)
+            it.setDisplayShowHomeEnabled(false)
+        }
+    }
+
+    private fun initInsetMargin() = with(binding) {
+        ViewCompat.setOnApplyWindowInsetsListener(coordinator) { v: View, insets: WindowInsetsCompat ->
+            val params = v.layoutParams as ViewGroup.MarginLayoutParams
+            params.bottomMargin = insets.systemWindowInsetBottom
+            toolbarContainer.layoutParams =
+                (toolbarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                    setMargins(0, insets.systemWindowInsetTop, 0, 0)
+                }
+            collapsingToolbarContainer.layoutParams =
+                (collapsingToolbarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                    setMargins(0, 0, 0, 0)
+                }
+
+            insets.consumeSystemWindowInsets()
+        }
+    }
+
+    private fun initScrollViewListeners() {
+        binding.scrollView.smoothScrollTo(0, 0)
+
+        binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val scrolledValue = binding.scrollView.scrollY
+            LogT.d("scrolledValue : $scrolledValue, ${150f.dpToPx(this@MotionIntroMainActivity).toInt()}")
+
+            if (scrolledValue > 150f.dpToPx(this@MotionIntroMainActivity).toInt()) {
+                if (isGatheringMotionAnimation.not()) {
+                    binding.gatheringDigitalThingsBackgroundMotionLayout.transitionToEnd()
+//                    binding.gatheringDigitalThingsMotionLayout.transitionToEnd()
+                    binding.buttonShownMotionLayout.transitionToEnd()
+                }
+            } else {
+                if (isGatheringMotionAnimation.not()) {
+                    binding.gatheringDigitalThingsBackgroundMotionLayout.transitionToStart()
+//                    binding.gatheringDigitalThingsMotionLayout.transitionToStart()
+                    binding.buttonShownMotionLayout.transitionToStart()
+                }
+            }
+
+            if (scrolledValue > binding.scrollView.height) {
+                if (isGatheringMotionAnimation.not()) {
+//                    binding.curationAnimationMotionLayout.setTransition(R.id.curation_animation_start1, R.id.curation_animation_end1)
+//                    binding.curationAnimationMotionLayout.transitionToEnd()
+//                    isCurationMotionAnimating = true
+                }
+            }
+        }
+    }
+
+    private fun initMotionLayoutListeners() {
+        binding.gatheringDigitalThingsBackgroundMotionLayout.setTransitionListener(object :
+            MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                isGatheringMotionAnimation = true
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) = Unit
+
+            override fun onTransitionCompleted(p0: MotionLayout?, currentId: Int) {
+                isGatheringMotionAnimation = false
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) =
+                Unit
+
+        })
+
+//        binding.curationAnimationMotionLayout.setTransitionListener(object : TransitionListener {
+//            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
+//
+//            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) = Unit
+//
+//            override fun onTransitionCompleted(p0: MotionLayout?, currentId: Int) {
+//                when (currentId) {
+//                    R.id.curation_animation_end1 -> {
+//                        binding.curationAnimationMotionLayout.setTransition(R.id.curation_animation_start2, R.id.curation_animation_end2)
+//                        binding.curationAnimationMotionLayout.transitionToEnd()
+//                    }
+//                }
+//            }
+//
+//            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
+//
+//        })
+    }
+
+}
+
+fun Float.dpToPx(context: Context): Float =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
+
+fun Activity.makeStatusBarTransparent() {
+    with(window) {
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        statusBarColor = Color.TRANSPARENT
     }
 }
